b70bef0610b7657edd7186d16901eaf93387ab0b
Manuel C Solis <manuelsolis@Manuels-MacBook-Air.local>
2018-07-31 19:53:04 -0400

    Created html for 2 players, click button functions are working.

--- public/main.js
+++ public/main.js
@@ -1,5 +1,51 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+
+
+
+const playerOneRockButtonClick = () => {
+  console.log('rock button is clicked')
+
+document.querySelector('.output').textContent = 'rock'
+
+}
+document.querySelector('.rock-button-player-one').addEventListener('click', playerOneRockButtonClick)
+
+const playerOnePaperButtonClick = () => {
+  console.log('paper button is clicked')
+  
+document.querySelector('.output').textContent = 'paper'
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.paper-button-player-one').addEventListener('click', playerOnePaperButtonClick)
+
+const playerOneScissorsButtonClick = () => {
+  console.log('scissors button is clicked')
+
+document.querySelector('.output').textContent = 'scissors'
+}
+
+document.querySelector('.scissors-button-player-one').addEventListener('click', playerOneScissorsButtonClick)
+
+const playerTwoRockButtonClick = () => {
+  console.log('rock button is clicked')
+
+document.querySelector('.player-two-output').textContent = 'rock'
+}
+
+document.querySelector('.rock-button-player-two').addEventListener('click', playerTwoRockButtonClick)
+
+const playerTwoPaperButtonClick = () => {
+  console.log('paper button is clicked')
+
+document.querySelector('.player-two-output').textContent = 'paper'
+}
+
+document.querySelector('.paper-button-player-two').addEventListener('click', playerTwoPaperButtonClick)
+
+const playerTwoScissorsButtonClick = () => {
+  console.log('scissors button is clicked')
+
+document.querySelector('.player-two-output').textContent = 'scissors'
+}  
+
+document.querySelector('.scissors-button-player-two').addEventListener('click', playerTwoScissorsButtonClick)
+
