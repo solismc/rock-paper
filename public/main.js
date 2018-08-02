35111667b8dbce23ce53106af0e62b6cfdb4e765
Manuel C Solis <manuelsolis@Manuels-MacBook-Air.local>
2018-08-02 13:39:12 -0400

    Player One field updates the output of the player

--- public/main.js
+++ public/main.js
@@ -4,7 +4,7 @@
 const playerOneRockButtonClick = () => {
   console.log('rock button is clicked')
 
-document.querySelector('.output').textContent = 'rock'
+document.querySelector('.answer-output').textContent = 'rock'
 
 }
 document.querySelector('.rock-button-player-one').addEventListener('click', playerOneRockButtonClick)
@@ -12,7 +12,7 @@ document.querySelector('.rock-button-player-one').addEventListener('click', play
 const playerOnePaperButtonClick = () => {
   console.log('paper button is clicked')
   
-document.querySelector('.output').textContent = 'paper'
+document.querySelector('.answer-output').textContent = 'paper'
 }
 
 document.querySelector('.paper-button-player-one').addEventListener('click', playerOnePaperButtonClick)
@@ -20,7 +20,7 @@ document.querySelector('.paper-button-player-one').addEventListener('click', pla
 const playerOneScissorsButtonClick = () => {
   console.log('scissors button is clicked')
 
-document.querySelector('.output').textContent = 'scissors'
+document.querySelector('.answer-output').textContent = 'scissors'
 }
 
 document.querySelector('.scissors-button-player-one').addEventListener('click', playerOneScissorsButtonClick)
@@ -51,7 +51,16 @@ document.querySelector('.scissors-button-player-two').addEventListener('click', 
 
 const playerOneNameButtonClick = () => {
   console.log('player name button is clicked')
-const playerOneName = document.querySelector('.player-name-one-input').textContent
+  const playerOneName = document.querySelector('.player-name-one-input').value
+  console.log(playerOneName)
+  document.querySelector('.player-one-name-output').textContent = playerOneName
 }
 document.querySelector('.player-one-name-button').addEventListener('click', playerOneNameButtonClick)
 
+const playerTwoNameButtonClick = () => {
+  console.log('player name button is clicked')
+  const playerTwoName = document.querySelector('.player-name-two-input').value
+  console.log(playerTwoName)
+  document.querySelector('.player-two-name-output').textContent = playerTwoName
+}
+document.querySelector('.player-two-name-button').addEventListener('click', playerTwoNameButtonClick)
